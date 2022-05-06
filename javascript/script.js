b62841f2b0c1cfab8ac98b1c83493e576e848ce5
santiagoZ013 <diazsantiagoponce1@gmail.com>
2022-05-06 15:09:59 -0500

    Primer reto terminado

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,45 @@
-let robertoInput = parseInt(document.getElementById('roberto'));
-let pedroInput = parseInt(document.getElementById('pedro'));
-let coflaInput = parseInt(document.getElementById('cofla'));
+let robertoInput = document.getElementById('roberto');
+let pedroInput = document.getElementById('pedro');
+let coflaInput = document.getElementById('cofla');
+let cambiotxt = document.getElementById('result3');
+let heladotxt = document.getElementById('result2');
+let resultadoCantidadtxt = document.getElementById('result1');
+
+let agua = 0.6, crema = 1, heladix = 1.6, heladovinch = 1.7, heraldo = 1.8, pote = 2.9;
+
+function mostrarCambio(helado, suma){
+    let cambio = suma-helado;
+    cambio = cambio.toFixed(2);
+    cambiotxt.innerText = `Tu cambio seria $${cambio}`
+}
 
 function mostrarResultados(){
-    let dineroRoberto = robertoInput.value;
-    let dineroPedro = pedroInput.value;
-    let dineroCofla = coflaInput.value;
+    let dineroRoberto = parseFloat(robertoInput.value);
+    let dineroPedro = parseFloat(pedroInput.value);
+    let dineroCofla = parseFloat(coflaInput.value);
     let suma = dineroRoberto+dineroCofla+dineroPedro;
-    let resultadoParrafo = document.getElementById('result');
-    resultadoParrafo.innerText = 'La cantidad total del dinero es $' + suma;
+    suma = suma.toFixed(2);
+    resultadoCantidadtxt.innerText = `La cantidad total del dinero es $${suma}`;
+    if(suma >= pote){
+        heladotxt.innerText = `El helado mas caro es el pote de helado 1/4 KG`
+        mostrarCambio(pote, suma);
+    }else if(suma >= heraldo){
+        heladotxt.innerText = `El helado mas caro es el helado heraldo`
+        mostrarCambio(heraldo, suma);
+    }else if(suma >= heladovinch){
+        heladotxt.innerText = `El helado mas caro es el helado heladovinch`
+        mostrarCambio(heladovinch, suma);
+    }else if(suma >= heladix){
+        heladotxt.innerText = `El helado mas caro es el helado heladix`
+        mostrarCambio(heladix, suma);
+    }else if(suma >= crema){
+        heladotxt.innerText = `El helado mas caro es el helado de crema`
+        mostrarCambio(crema, suma);
+    }else if(suma >= agua){
+        heladotxt.innerText = `El helado mas caro es el helado de agua`
+        mostrarCambio(agua, suma);
+    }else{
+        heladotxt.innerText = `No te alcansa para ningun helado`
+        cambiotxt.innerText = `Te faltan $${(agua-suma).toFixed(2)}`
+    }
 }
